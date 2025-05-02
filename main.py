7440b05b485795b07ec1045fed96804fb997db49
username <123@>
2025-05-03 03:32:55 +0500

    проверка по дням недели

--- main.py
+++ main.py
@@ -15,12 +15,41 @@ def hh_mm():
             print('формат должен быть ЧЧ:ММ')
             return hh_mm()
 def signal_type():
-    st = input('Звуковое уведомление? y/n или название файла из каталога: ')
-    if st == 'n' or st == '':
+    st = input('Звуковое уведомление (y/n или название файла из каталога): ')
+    if st.lower() == 'n' or st == '':
         st = None
-    elif st == 'y':
+    elif st.lower() == 'y':
         st = 'default'
     return st
+def repeat():
+    rp = input('Повторять (y/n или дни недели через пробел): ')
+    if rp.lower() == 'n' or rp == '':
+        return False
+    elif rp.lower() == 'y':
+        return {0, 1, 2, 3, 4, 5, 6}
+    else:
+        wkl = set()
+        for d in rp.split(' '):
+            if d.lower() in ['0', 'понедельник', 'пн', 'monday', 'mon', 'mn']:
+                wkl.add(0)
+            elif d.lower() in ['1', 'вторник', 'вт', 'tuesday', 'tue', 'tu']:
+                wkl.add(1)
+            elif d.lower() in ['2', 'среда', 'ср', 'wednesday', 'wed', 'wd']:
+                wkl.add(2)
+            elif d.lower() in ['3', 'четверг', 'чтв', 'чт', 'thursday', 'thu', 'th']:
+                wkl.add(3)
+            elif d.lower() in ['4', 'пятница', 'пт', 'friday', 'fri', 'fr']:
+                wkl.add(4)
+            elif d.lower() in ['5', 'суббота', 'сбт', 'сб', 'saturday', 'sat', 'sa']:
+                wkl.add(5)
+            elif d.lower() in ['6', 'воскресенье', 'вс', 'sunday', 'sun', 'sn']:
+                wkl.add(6)
+            else:
+                print('Неправильный формат ввода')
+                break
+        else:
+            return wkl
+        return repeat()
 def alarm(signal):
     print(f'{time.strftime('%H:%M')} Время вставать!')
     if signal:
@@ -35,19 +64,27 @@ while True:
     if not cl:
         break
     elif all_tms.get(cl[0]):
-        all_tms[cl[0]][cl[1]] = signal_type()
+        all_tms[cl[0]][cl[1]] = (signal_type(), repeat())
     else:
-        all_tms[cl[0]] = {cl[1] : signal_type()}
+        all_tms[cl[0]] = {cl[1] : (signal_type(), repeat())}
 while True:
+    cur_wd = time.localtime().tm_wday
     cur_hr = time.localtime().tm_hour
     cur_mn = time.localtime().tm_min
     cur_sc = time.localtime().tm_sec
     if not all_tms:
-        print('Будильник не установлен!')
         break
     elif cur_hr in all_tms:
-        if cur_mn in all_tms[cur_hr]:
-            alarm(all_tms[cur_hr][cur_mn])
+        if not all_tms[cur_hr]:
+            del all_tms[cur_hr]
+            continue
+        elif cur_mn in all_tms[cur_hr]:
+            if not all_tms[cur_hr][cur_mn][1]:
+                alarm(all_tms[cur_hr][cur_mn][0])
+                del all_tms[cur_hr][cur_mn]
+                continue
+            elif cur_wd in all_tms[cur_hr][cur_mn][1]:
+                alarm(all_tms[cur_hr][cur_mn][0])
         time.sleep(60 - cur_sc)
     else:
         time.sleep((60 - cur_mn) * 60 - cur_sc)
